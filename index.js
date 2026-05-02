8350ff4bd13969f94c6ecc3ee3e13ef667268c30
Diego Tapia <409595@sistemas.frc.utn.edu.ar>
2026-05-02 00:18:55 -0300

    doc: Quedo la estructura para el desafio N°1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo comenzemos por este script primero");
+"use strict";
+import seedrandom from "seedrandom";
+
+var random = seedrandom(1763519);
+
+let numerosAleatorios = Array.from({length: 100}, () => random.int32());
+
+console.log("Demostracion de Join: {", numerosAleatorios); // De esta forma mostrara un array en formato json
+
+console.log("Mostramos la contidad de numeros positivos: ", numerosAleatorios.filter(value => value > 0).length);
+
+let numerosAleatoriosPositivos = numerosAleatorios.filter(value => value > 0);
+
+console.log("Mostramos los numeros positivos: { " + numerosAleatoriosPositivos.join(",") + " }"); // Y de esta forma mostrara el Array como una concatenacion
